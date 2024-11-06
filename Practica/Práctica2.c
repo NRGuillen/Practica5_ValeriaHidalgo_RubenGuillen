bf147d2d92880a3dc8dbcc8399b0787b2202986b
Hidalgo002 <vhidalgodominguez@gmail.com>
2024-11-06 10:25:44 +0100

    “Actualizacion”

--- Practica/Práctica2.c
+++ Practica/Práctica2.c
@@ -6,23 +6,27 @@ int main(){
 	
 #define MÍNIMO 5.0
 
-	double notas[5];
+	int alumnos;
+	printf("Introduzca el número de almunos a evaluar: ");
+	scanf("%d", &alumnos);
+	
+	double notas[alumnos];
 	double suma = 0;
-	for (int i = 0; i<5; i++){ //Se va aumentando en 1 y se va moviendo la posición del array, se va guardando en nota[0], luego nota[1]... hasta 4
+	for (int i = 0; i<alumnos; i++){ //Se va aumentando en 1 y se va moviendo la posición del array, se va guardando en nota[0], luego nota[1]... hasta 4
 	printf("Introduce la nota: ");//Pedimos la nota
 	scanf("%lf", &notas[i]); //Mandamos las notas introducidas en el teminal al array de notas
 	suma+= notas[i]; //Suma de las notas, se almacenan en suma
 	}
 
 	//PROMEDIO
-	double promedio = suma / 5; //Para el promedio, creo una variable que se llama 'promedio' en la que almaceno la suma de todas las notas y las divido entre 5.
+	double promedio = suma / alumnos; //Para el promedio, creo una variable que se llama 'promedio' en la que almaceno la suma de todas las notas y las divido entre 5.
 	printf("Promedio de notas: %.2lf\n", promedio);
 	
 	//NÚMERO DE APROBADOS Y SUSPENSOS
 	int aprobado = 0;
 	int suspenso = 0;
 
-	for(int i = 0; i<5;i++){ //El bucle va cambiando la posición del almacenamiento del array
+	for(int i = 0; i<alumnos;i++){ //El bucle va cambiando la posición del almacenamiento del array
 	if(notas[i] >= MÍNIMO){ //Si la casilla es mayor o igual que el mínimo, entonces, a la varible de aprobado, se le suma 1 para aprobado, si no, se le suma 1 a suspendido
 	aprobado += 1;}
 	else{
@@ -37,7 +41,7 @@ int main(){
 	float min = notas[0];
 	int min_posicion = 0;
 
-	for(int i=1;i<5;i++){ //int= 1 porque empezamos a comparar el notas[0] con el notas [1]
+	for(int i=1;i<alumnos;i++){ //int= 1 porque empezamos a comparar el notas[0] con el notas [1]
 		if(notas[i] > max){ //Si el dato del array es mayor al máximo, que inicializa con el valor de la primera casilla, es mayor, etonces, a máximo se le asigna un nuevo valor y se guarda la posicón en max_posicion
 		max = notas[i];
 		max_posicion = i;}
@@ -51,7 +55,7 @@ int main(){
 
 	//NOTAS POR ENCIMA DEL PROMEDIO
 	int alto_prom = 0; 
-	for(int i=0;i<5;i++){ 
+	for(int i=0;i<alumnos;i++){ 
                 if(notas[i] > promedio){
 		alto_prom = i;	//SI es mayor que el promedio, se guarda el i en alto_prom
 		printf("Estudiante en la posición %d: %.2lf\n", alto_prom + 1, notas[i]);
---
+++ Practica/Práctica3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+	int alumnos;
+	printf("Introduce el número de al"
+
+
+return 0;
+}
