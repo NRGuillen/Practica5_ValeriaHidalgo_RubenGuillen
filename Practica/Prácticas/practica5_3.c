a5a3c2b9db4d083445bb5c73f4ef26e06bded2a5
Hidalgo002 <vhidalgodominguez@gmail.com>
2024-11-06 13:36:51 +0100

    “Actualizacion”

--- Practica/Prácticas/practica5_3.c
+++ Practica/Prácticas/practica5_3.c
@@ -21,8 +21,7 @@ printf("Solo está permitida la gestión de 10 productos a la vez \n");
 
         for(int i = 0; i<productos; i++) { //Repetiremos el bucle de preguntarle el nombre de los productos hasta que i sea mayor que los productos
         printf("Introduce el nombre del producto: ");                                        scanf("%s", lista_datos[i].nombre);} //Cada producto se guarda en lista_datos[0] para el primero, y en el apartado de nombre especificamente. El siguiente prodcuto se guardará en lista_datos[1]... Así hasta que i sea mayor que el nº de productosque ingresó el usuario al principio
-
-}
+                                                                                    }
 
 
 return 0;
