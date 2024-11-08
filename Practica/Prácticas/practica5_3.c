98132bed51ed2038b7cd599824e31e72e6bac219
Hidalgo002 <vhidalgodominguez@gmail.com>
2024-11-08 14:29:16 +0100

    “Actualizacion”

--- Practica/Prácticas/practica5_3.c
+++ Practica/Prácticas/practica5_3.c
@@ -1,28 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct datos_productos{
-        char nombre[20];
-        int cantidad;
-
-};
-
+#define PRODUCTOS 10
+#define LONGITUD_NOMBRE 21
+#define MAX_PROD 1000
 int main(){
-        //PEDIR EL NÚMERO DE PRODUCTOS AL USUARIO
-        int productos;
-        printf("Introduce el número total de productos a gestionar: ");
-        scanf("%d", &productos);
-        if(productos > 10){
-printf("Solo está permitida la gestión de 10 productos a la vez \n");
-        }
-        else {
+	int num;
+	char nombre[PRODUCTOS][LONGITUD_NOMBRE];	
+	int cantidad[MAX_PROD];
 
-        struct datos_productos lista_datos[productos]; //Arreglo para traer el struct al main
-
-        for(int i = 0; i<productos; i++) { //Repetiremos el bucle de preguntarle el nombre de los productos hasta que i sea mayor que los productos
-        printf("Introduce el nombre del producto: ");                                        scanf("%s", lista_datos[i].nombre);} //Cada producto se guarda en lista_datos[0] para el primero, y en el apartado de nombre especificamente. El siguiente prodcuto se guardará en lista_datos[1]... Así hasta que i sea mayor que el nº de productosque ingresó el usuario al principio
-                                                                                    }
-
-
+	//1.Pedimos al usuario la cantidad de productos que va a gestionar
+	printf("Introduce el número de prodcutos que va a gestionar: ");
+	scanf(" %d", &num); 
+	
+	//2.
+	if(num > 10){
+		printf("Solo se pueden gestionar 10 productos a la vez.\n");}
+	else{
+		for(int i = 0; i<num; i++){
+	printf("Introduce el nombre de los productos: ");
+	scanf(" %s", nombre[i]);
+	}
+		for(int i = 0; i<num; i++){
+	printf("Introduce la cantidad de cada producto: ");
+        scanf(" %d", &cantidad[i]);}
+	
+	}
+	//INVENTARIO:
+	printf("Inventario Cpmleto:\n");
+	for(int i = 0;i<num; i++){
+	printf("\tProducto: %s - %d\n", nombre[i], cantidad[i])			
+	;}
 return 0;
 }
+
