f38144e92abfa7d01bd1109eee7ed5fc83251819
Hidalgo002 <vhidalgodominguez@gmail.com>
2024-11-12 13:41:28 +0100

    “Actualizacion”

--- Practica/Prácticas/practica5_3.c
+++ Practica/Prácticas/practica5_3.c
@@ -14,10 +14,10 @@ int main(){
 
 	//1.Pedimos al usuario la cantidad de productos que va a gestionar
 	printf("Introduce el número de prodcutos que va a gestionar: ");
-	scanf(" %d", &num);
+	scanf("%d", &num);
 
-	//2.
-	if(num >= 10){
+	//2. Recogemos el número de productos con los que quiere hacer el inventario
+	if(num > PRODUCTOS){
 		printf("Solo se pueden gestionar 10 productos a la vez.\n");}
 	else{
 		for(int i = 0; i<num; i++){
@@ -42,29 +42,28 @@ int main(){
 	scanf(" %s", respuesta);
 
 	int mod; //Almacena el número de objetos a modificar en el inventario.
-	if (strcmp(respuesta, "no") == 0 || strcmp(respuesta, "No") == 0){
+	if (strcmp(respuesta, "no") == 0){
 	printf("No se procederá al reabastecimiento del producto.\n");}
 
-	else if (strcmp(respuesta, "si") == 0 || strcmp(respuesta, "Sí") == 0 || strcmp(respuesta, "Sí")|| strcmp(respuesta, "Sí")){
+	else(strcmp(respuesta, "sí") == 0);{
 		printf("Introduzca el número de productos que va a modificar:  ");
 		scanf(" %d", &mod);}
 
-		if (mod >= 10){
+	if (mod > PRODUCTOS){
                 printf("Solo se pueden gestionar 10 productos a la vez.\n");}
-        	else{
-                	for(int i = 0; i<mod; i++){
-        		printf("Introduce el nombre del producto %d: ", i + 1 );
-        		scanf(" %s", nombre[i]);
+        
+	else{
+                for(int j = 0; j<mod; j++){ //Volvemos a pedir el nombre del producto
+        	printf("Ingrese el nombre del producto: ");
+        	scanf(" %s", nombre[j]);
 
-        		printf("Introduce la cantidad a añadir del producto %s: ", nombre[i]);
-       			scanf(" %d", &cantidadnueva[i]);
-			int suma = cantidadnueva[i] + cantidad[i];
-			printf("La cantidad nueva es: %d ", cantidadnueva[i]);
-			}}
-
-
-
-	}
+        	printf("Cantidad a añadir: ");
+       		scanf(" %d", &cantidadnueva[j]);}//Guardamos la cantidad a sumar
+			
+			
+		for(int j = 0; j<mod; j++){		
+		int suma = cantidadnueva[j] + cantidad[j];
+		printf(" %d", cantidadnueva[j]);}}	
 return 0;
 }
 
